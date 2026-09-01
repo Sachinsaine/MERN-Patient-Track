45c9d29ae4e87464934b87dbf6ea4668733dca5c
Ronnie <royalronnie05@gmail.com>
2026-09-01 22:44:21 +0530

    skeletons added

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,12 +3,29 @@ import { PatientProfile } from "../PatientProfile/PatientProfile";
 import { Patients } from "../Patients/Patients";
 import styles from "./dashboard.module.css";
 
+// export const Dashboard = () => {
+//   return (
+//     <div className={styles.dashboard}>
+//       <Patients />
+//       <DiagnosisHistory />
+//       <PatientProfile />
+//     </div>
+//   );
+// };
 export const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
-      <Patients />
-      <DiagnosisHistory />
-      <PatientProfile />
+      <div className={styles.patients}>
+        <Patients />
+      </div>
+
+      <div className={styles.diagnosis}>
+        <DiagnosisHistory />
+      </div>
+
+      <div className={styles.profile}>
+        <PatientProfile />
+      </div>
     </div>
   );
 };

7de8c6bfcd89e6abe6b928c7a5f571848a61d6ea
Ronnie <royalronnie05@gmail.com>
2026-09-22 19:43:57 +0530

    productio CORS fixed

--- Backend/server.js
+++ Backend/server.js
@@ -15,10 +15,16 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-patient-track-axkc.vercel.app",
     credentials: true,
   }),
 );
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
 app.use(express.json());
 app.use(cookieParser());
 

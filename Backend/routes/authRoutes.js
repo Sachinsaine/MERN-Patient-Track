96b24e12bfcf5be9a57cae05702c41e09649bf5c
Ronnie <royalronnie05@gmail.com>
2026-09-23 15:25:51 +0530

    login-logout fixed

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -100,8 +100,8 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // development
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 60 * 60 * 1000,
     });
 
--- Backend/server.js
+++ Backend/server.js
@@ -14,20 +14,20 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 // for Deployement
-// app.use(
-//   cors({
-//     origin: "https://mern-patient-track-axkc.vercel.app",
-//     credentials: true,
-//   }),
-// );
-
-//For local
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-patient-track-axkc.vercel.app",
     credentials: true,
   }),
 );
+
+//For local
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
 app.use(express.json());
 app.use(cookieParser());
 

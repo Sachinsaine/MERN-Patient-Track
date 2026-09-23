d72678425769f3d6267325f524b7a87e2aa50b10
Ronnie <royalronnie05@gmail.com>
2026-09-23 15:28:16 +0530

    CORS fixed

--- Backend/server.js
+++ Backend/server.js
@@ -13,21 +13,16 @@ const multer = require("multer");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-// for Deployement
 app.use(
   cors({
-    origin: "https://mern-patient-track-axkc.vercel.app",
+    origin: [
+      "https://mern-patient-track-axkc.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   }),
 );
 
-//For local
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// );
 app.use(express.json());
 app.use(cookieParser());
 
